039b390a2fb30c60a13ca8ac662d5e008a8af4f7
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-07-27 06:54:25 +0200

    Delete MongoDB.js file from model

--- model/MongoDB.js
+++
@@ -1,8 +0,0 @@
-// packeges:
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.DB_ACCESS,()=>{
-    console.log('The DataBase is here ....')
-})
-
-module.exports = mongoose;
